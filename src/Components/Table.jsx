f9a0e22aaccfde4b1e99d7ae7dc9118c1b21abfa
Jrios <Jesusfernandorios93@gmail.com>
2021-03-15 21:06:03 -0700

    will work on console warnings. setting up proper DOMrender methods.

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -5,9 +5,9 @@ import './style/table.css'
 function Table({countries}) {
     return (
         <div className="table">
-            {countries.map( ({country, cases}) => {
+            {countries.map( ({country, cases}, index) => {
                return(
-               <tr>
+               <tr key={index}>
                     <td>{country}</td>
                     <td><strong>{numeral(cases).format('0,0')}</strong></td>
                 </tr>
